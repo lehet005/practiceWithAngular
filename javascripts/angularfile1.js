46078e56b238417aaa16d5fad40f0f11d223bafe
joseph <thele116@morris.umn.edu>
2014-09-11 14:52:07 -0500

    Coppied controller from todo.js into angularfile1.js. This is messy but allows proper functionality until such time as we find a good way to have multiple apps per page.

--- javascripts/angularfile1.js
+++ javascripts/angularfile1.js
@@ -1,4 +1,6 @@
 angular.module('cougarApp', [])
+
+    // controller for the list in the left hand column of index.html
     .controller('CougarController', ['$scope', function($scope){
 
         $scope.cougarItems = [
@@ -26,4 +28,33 @@ angular.module('cougarApp', [])
             });
         };
 
+    }])
+
+    //controller for todos in the right hand column of index.html
+    .controller('TodoController', ['$scope', function($scope) {
+        $scope.todos = [
+            {text:'learn angular', done:true},
+            {text:'build an angular app', done:false}];
+
+        $scope.addTodo = function() {
+            $scope.todos.push({text:$scope.todoText, done:false});
+            $scope.todoText = '';
+        };
+
+        $scope.remaining = function() {
+            var count = 0;
+            angular.forEach($scope.todos, function(todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
+
+        $scope.archive = function() {
+            var oldTodos = $scope.todos;
+            $scope.todos = [];
+            angular.forEach(oldTodos, function(todo) {
+                if (!todo.done) $scope.todos.push(todo);
+            });
+        };
     }]);
+
